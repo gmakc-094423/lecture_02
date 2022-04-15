2d6ea5c026a9c76534301277985ea285b747adeb
gmakc-094423 <gmakc-094423@yandex.ru>
2022-04-16 00:03:13 +0300

    очередное сохранение

--- seminar_02/task2/Program.cs
+++ seminar_02/task2/Program.cs
@@ -4,12 +4,15 @@
 
 
 Console.Write("Введи 1 число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введи 2 число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
 
-void Result(int number1, int number2){
-  int divNumber = number1 % number2;
+int[] array = new int[2];
+
+array[0] = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи 2 число: ");
+array[1] = Convert.ToInt32(Console.ReadLine());
+
+void Result(int[] array){
+  int divNumber = array[0] % array[1];
   if (divNumber == 0)
   {
     Console.WriteLine("Кратные числа ");
@@ -19,12 +22,21 @@ void Result(int number1, int number2){
   }
 }
 
-if (number1 < number2){
-  int num1 = number2;
-  int num2 = number1;
+int[] Comparision(int[] array){
+  int[] array1 = new int[2];
+  if (array[0] > array[1]){
+    array1[0] = array[0];
+    array1[1] = array[1];
+  }
+  else {
+    array1[0] = array[1];
+    array1[1] = array[0];
+  }
+  return array1;
 }
-else {
-  int num1 = number1;
-  int num2 = number2;
-} 
-Result(num1, num2);
+
+int[] array1 = Comparision(array);
+
+Result(array1);
+ 
+
